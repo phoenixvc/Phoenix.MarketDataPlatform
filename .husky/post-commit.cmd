eca885fc82b46e96bca877a569161318e11414b1
JustAGhosT <smit.jurie@gmail.com>
2025-05-22 23:58:19 +0200

    Test post-commit with HTML reports

--- .husky/post-commit.cmd
+++ .husky/post-commit.cmd
@@ -1,4 +1,10 @@
 @echo off
 echo Running post-commit tests...
 npm run postcommit || exit /b 0
+
+echo Generating coverage reports...
+dotnet tool install -g dotnet-reportgenerator-globaltool --skip-duplicate
+reportgenerator "-reports:tests/**/TestResults/**/coverage.cobertura.xml" "-targetdir:coverage-report" "-reporttypes:Html" || exit /b 0
+
+echo Coverage report generated in coverage-report directory
 exit /b 0
